ddee200b0d09fab84e91d597f8a1ffccdbba1abf
이주성 <32183520@dankook.ac.kr>
2024-05-17 17:26:12 +0900

    Security 설정

--- dl-auth/src/main/java/com/example/dlauth/domain/repository/MemberRepository.java
+++ dl-auth/src/main/java/com/example/dlauth/domain/repository/MemberRepository.java
@@ -1,11 +1,14 @@
 package com.example.dlauth.domain.repository;
 
+import com.example.dlauth.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.lang.reflect.Member;
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByStudentCode(String studentCode);
 
 }
