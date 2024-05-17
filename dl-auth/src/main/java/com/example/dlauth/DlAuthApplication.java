2030b28f1a2e0722ae495d3ddd9ceb500c02ec6e
이주성 <32183520@dankook.ac.kr>
2024-05-17 16:20:15 +0900

    프로젝트 초기 세팅

---
+++ dl-auth/src/main/java/com/example/dlauth/DlAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.dlauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DlAuthApplication.class, args);
+    }
+
+}
---
+++ dl-auth/src/test/java/com/example/dlauth/DlAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dlauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
